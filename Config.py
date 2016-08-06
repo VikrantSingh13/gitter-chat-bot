5a0f2a86ac6ffaf8f3afd61ce64ae0634370f182
Vikrant Singh <vikrantsinghvm@gmail.com>
2016-08-06 03:18:46 -0400

    Added targeted message feature

--- Config.py
+++ Config.py
@@ -1,10 +1,10 @@
 
 HOST = "irc.gitter.im"
 USER = "VikrantSingh13"
-PASS = ""
+PASS = "69d459fd056e8264f2b86ef0509d539f656a91da"
 PORT = 6697
 ROOM = "FreeCodeCamp/FreeCodeCamp"
 
-TARGET_MESSAGE = "how are you?"
-REPLY_MESSAGE = "I am doing great. How are you?"
+TARGET_MESSAGE = "hello world"
+REPLY_MESSAGE = "Hello! Welcome to FCC."
 INITIALIZATION_MESSAGE = ""
