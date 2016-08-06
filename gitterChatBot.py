ef0580a7eea63939d33651871d5904f42bc2dd7c
Vikrant Singh <vikrantsinghvm@gmail.com>
2016-08-06 03:20:46 -0400

    Stay connected with Ping-Pong :D

--- gitterChatBot.py
+++ gitterChatBot.py
@@ -13,7 +13,17 @@ while(True):
 
   for line in temp:
     print(line)
+
+    #Pong the server when needed
     if line[0:4] == "PING":
       send_pong(s, line[4:])
-  
-  time.sleep(3)
+      break
+
+    #extract the user and message from the response line
+    temp_message = get_user_message(line)
+    user = temp_message[0]
+    message = temp_message[1]
+
+    if TARGET_MESSAGE in message:
+      send_message(s, "@" + user + " " + REPLY_MESSAGE)
+  time.sleep(0.3)
